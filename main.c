5734fbda6188b8b54a58af3cb8089be1ef8967eb
eastack <admin@eastack.me>
2021-05-12 21:31:51 +0800

    feat(ui): add support for keyboard operation

--- main.c
+++ main.c
@@ -2,12 +2,30 @@
 #include <ddcutil_c_api.h>
 #include "ddc.h"
 
+bool scale_pressed = false;
+
+static gboolean
+scale_press() {
+    scale_pressed = true;
+    return FALSE;
+}
+
 static void
-scale_released(GtkRange *range,
-               GdkEventButton *event,
-               DDCA_Display_Handle *dh) {
+value_changed(GtkRange *range,
+              DDCA_Display_Handle *dh) {
+    if (!scale_pressed) {
+        gdouble pos = gtk_range_get_value(range);
+        set_display_brightness(dh, (int) pos);
+    }
+}
+
+static void
+scale_release(GtkRange *range,
+              GdkEventButton *event,
+              DDCA_Display_Handle *dh) {
     gdouble pos = gtk_range_get_value(range);
     set_display_brightness(dh, (int) pos);
+    scale_pressed = false;
 }
 
 static void
@@ -25,17 +43,28 @@ activate(GtkApplication *app,
     gtk_range_set_value(GTK_RANGE(scale), brightness);
     gtk_widget_set_hexpand(scale, TRUE);
     gtk_widget_set_valign(scale, GTK_ALIGN_START);
+
     g_signal_connect (scale,
-                     "button-release-event",
-                     G_CALLBACK(scale_released),
-                     dh);
+                      "button-press-event",
+                      G_CALLBACK(scale_press),
+                      NULL);
+
+    g_signal_connect (scale,
+                      "value-changed",
+                      G_CALLBACK(value_changed),
+                      dh);
+
+    g_signal_connect (scale,
+                      "button-release-event",
+                      G_CALLBACK(scale_release),
+                      dh);
 
     // grid
     GtkWidget *grid;
     grid = gtk_grid_new();
     gtk_grid_set_column_spacing(GTK_GRID (grid), 1);
     gtk_grid_set_column_homogeneous(GTK_GRID (grid), TRUE);
-    gtk_grid_attach(GTK_GRID(grid), scale, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID (grid), scale, 0, 0, 1, 1);
 
     // assembly
     gtk_container_add(GTK_CONTAINER (window), grid);
@@ -47,19 +76,16 @@ activate(GtkApplication *app,
 int
 main(int argc,
      char **argv) {
-    // gtk
-    GtkApplication *app;
-    int status;
     // ddcutil
     DDCA_Display_Handle *dh = open_display_by_dispno(1);
-
-    app = gtk_application_new("me.eastack.ddc-light", G_APPLICATION_FLAGS_NONE);
+    GtkApplication *app = gtk_application_new("me.eastack.ddc-light", G_APPLICATION_FLAGS_NONE);
     g_signal_connect (app, "activate", G_CALLBACK(activate), dh);
-    status = g_application_run(G_APPLICATION (app), argc, argv);
-    g_object_unref(app);
 
+    int status = g_application_run(G_APPLICATION (app), argc, argv);
+
+    g_object_unref(app);
     ddca_close_display(dh);
-    printf("Closed display");
+
     return status;
 }
 
